eef35a312da0d9e56e9d5998dbf9342cc8998c8d
Ayanou Prince <princeayanou@gmail.com>
2026-07-29 15:49:01 +0100

    création du dto pour créer un user

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,34 @@
-export class CreateUserDto {}
+import { z } from 'zod';
+import { createZodDto } from 'nestjs-zod';
+import { user_statut } from '../../generated/prisma/client';
+
+export const CreateUserSchema = z.object({
+  nom: z
+    .string({
+      message: 'Le nom est requis',
+    })
+    .min(3, 'Le nom doit contenir au moins 3 caractères'),
+
+  prenoms: z
+    .string({
+      message: 'Le prénom est requis',
+    })
+    .min(3, 'Le prénom doit contenir au moins 3 caractères'),
+
+  email: z
+    .string({
+      message: "L'email est requis",
+    })
+    .email("Format d'email invalide"),
+
+  telephone: z
+    .string({
+      message: 'Le numéro de téléphone est requis',
+    })
+    .min(10, 'Numéro de téléphone trop court'),
+
+  statut: z.enum(user_statut).optional().default(user_statut.ACTIF),
+});
+
+// 2. Création de la classe DTO NestJS à partir du schéma Zod
+export class CreateUserDto extends createZodDto(CreateUserSchema) {}
